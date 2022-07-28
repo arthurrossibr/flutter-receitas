90bfaffb33b2339a8c5d531ced5676df30c763c0
arthurrossicardoso <arthurrossi15@gmail.com>
2022-07-27 21:42:33 -0300

    Add decoration to category_item.dart

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -10,6 +10,17 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
       child: Text(category.title),
     );
   }
